010b611ef758381357af863393ca3d09fd5d0f93
runpatrick <34929307+runpatrick@users.noreply.github.com>
2021-09-18 20:23:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 import flask
+from flask import request
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Heruko"
+  host = request.args.get('host')
+  length = request.args.get('length')
+  if host == None:
+    print("Please fill out the required!");
+  else:
+    print(host);
+  if length == None:
+    print("Please fill out the required!");
+  else:
+    print(length);
+    execute();
+  return render_template("index.html")

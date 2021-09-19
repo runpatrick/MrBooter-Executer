bd7551c41cf998669d5bb1d59ed25cc3f10b7979
runpatrick <34929307+runpatrick@users.noreply.github.com>
2021-09-19 01:17:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def execute():
   options.add_argument(f'user-agent={userAgent}')
   driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH , chrome_options=options)
   driver.get("https://katacoda.com/embed/runpatrick/courses/challenges/node-challenge-1/?v=2&embed=true&ui=inline&host=137.184.30.68&url=http%3A%2F%2F137.184.30.68%2Ftest%2F%3Fhost%3D1&target=katacoda-scenario-1&nonce=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWJlZCI6dHJ1ZSwiZG9tYWluIjoiaHR0cDovLzEzNy4xODQuMzAuNjgvIiwiaXAiOiIyNC4yMjguMTA5LjI1NSIsImlhdCI6MTYzMTg2NTgzMiwiZXhwIjoxNjMxODY1ODYyfQ.DA-Sa33cRPy1IrloJNxqvIs37wotHl6KdSgyzCNYNPg&color=004d7f")
-  time.sleep(7)
-  driver.save_screenshot("katacoda.png")
+  time.sleep(5)
   url = driver.execute_script("return document.getElementById('weburl').innerText;")
   print(url)

6c957dc93b84c7fc086ce30a2a80ee76189b650c
runpatrick <34929307+runpatrick@users.noreply.github.com>
2021-09-18 20:41:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ def index():
   host = request.args.get('host')
   length = request.args.get('length')
   if host == None:
-    return("Please fill out the required!");
+    print("Please fill out the required!");
   else:
-    return(host);
+    print(host);
   if length == None:
-    return("Please fill out the required!");
+    print("Please fill out the required!");
   else:
-    return(length);
+    print(length);
     test();
   return "{Executer: Online}"
 
 def test():
-  return "It worked!"
+  print "It worked!"
 
